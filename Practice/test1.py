5c27053b083597227be621b147a002daecc8c928
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-04-28 18:11:28 -0400

    Initial commit

---
+++ Practice/test1.py
@@ -0,0 +1,15 @@
+from pyquil         import Program, get_qc
+from pyquil.gates   import *
+from pyquil.api     import local_forest_runtime
+
+p = Program(H(0), CNOT(0,1))
+print(p)
+p += X(1)
+print(p)
+
+with local_forest_runtime():
+    qc = get_qc('9q-square-qvm')
+    result = qc.run_and_measure(p, trials=10)
+    print(result[0])
+    print(result[1])
+    print(len(result))
