2f41f761dfeb926b3eec2d41ccbfc3ce25b10bc9
Bryan Pan <bryanpan342@gmail.com>
2020-05-07 15:03:34 -0700

    added some npy files so that we dont have to keep generating

--- HW1/dj.py
+++ HW1/dj.py
@@ -2,33 +2,5 @@ import numpy as np
 import itertools
 import oracle
 
-# Unit Test Functions
-
-def is_bal_or_const(m, f):
-    if f is oracle.DJ.CONSTANT:
-        x = next(iter(m.values()))
-        return all(val == x for val in m.values())
-    elif f is oracle.DJ.BALANCED:
-        val0 = sum(value == '0' for value in m.values())
-        val1 = sum(value == '1' for value in m.values())
-        return val0 == val1 and val0+val1 == len(m.values())
-
-def is_unitary(m):
-    # https://stackoverflow.com/a/34997613
-    return np.allclose(np.eye(len(m)), m.dot(m.T.conj()))
-
-def unit_tests(n):
-    const_mapping = oracle.init_bit_mapping(n, dj=True, func=oracle.DJ.CONSTANT)
-    assert is_bal_or_const(const_mapping, oracle.DJ.CONSTANT)
-
-    balanced_mapping = oracle.init_bit_mapping(n, dj=True, func=oracle.DJ.BALANCED)
-    assert is_bal_or_const(balanced_mapping, oracle.DJ.BALANCED)
-
-    U_const_f = oracle.gen_matrix(const_mapping, n, 1)
-    assert is_unitary(U_const_f)
-
-    U_bal_f = oracle.gen_matrix(balanced_mapping, n, 1)
-    assert is_unitary(U_bal_f)
-
-for n in range(5):
-    unit_tests(n+1)
+from pyquil import Program, get_qc
+from pyquil.gates import *
---
+++ HW1/dj_ut.py
@@ -0,0 +1,54 @@
+import numpy as np
+import os
+import itertools
+import oracle
+
+# Unit Test Functions
+
+def is_bal_or_const(m, f):
+    if f is oracle.DJ.CONSTANT:
+        x = next(iter(m.values()))
+        return all(val == x for val in m.values())
+    elif f is oracle.DJ.BALANCED:
+        val0 = sum(value == '0' for value in m.values())
+        val1 = sum(value == '1' for value in m.values())
+        return val0 == val1 and val0+val1 == len(m.values())
+
+def is_unitary(m):
+    # https://stackoverflow.com/a/34997613
+    return np.allclose(np.eye(len(m)), m.dot(m.T.conj()))
+
+def unit_tests(n):
+    if not os.path.exists('uf'):
+        os.mkdir('uf') 
+    SAVEPATH = 'uf/'
+    CONSTPATH = 'const'+str(n)
+    BALPATH = 'bal'+str(n)
+    
+    if not os.path.exists(SAVEPATH + CONSTPATH+'.npy'):
+        const_mapping = oracle.init_bit_mapping(n, dj=True, func=oracle.DJ.CONSTANT)
+        assert is_bal_or_const(const_mapping, oracle.DJ.CONSTANT)
+        U_const_f = oracle.gen_matrix(const_mapping, n, 1)
+    else:
+        U_const_f = np.load(SAVEPATH + CONSTPATH +'.npy')
+
+    if not os.path.exists(SAVEPATH + BALPATH+'.npy'):
+        balanced_mapping = oracle.init_bit_mapping(n, dj=True, func=oracle.DJ.BALANCED)
+        assert is_bal_or_const(balanced_mapping, oracle.DJ.BALANCED)
+        U_bal_f = oracle.gen_matrix(balanced_mapping, n, 1)
+    else:
+        U_bal_f = np.load(SAVEPATH + BALPATH+'.npy')
+
+    assert is_unitary(U_const_f)
+    if not os.path.exists(SAVEPATH + CONSTPATH+'.npy'):
+        np.save(SAVEPATH + CONSTPATH, U_const_f)
+
+    assert is_unitary(U_bal_f)
+    if not os.path.exists(SAVEPATH + CONSTPATH+'.npy'):
+        np.save(SAVEPATH + BALPATH, U_bal_f)
+
+
+for n in range(1,10):
+    unit_tests(n)
+print(np.load('uf/bal3.npy'))
+print(np.load('uf/const3.npy'))
