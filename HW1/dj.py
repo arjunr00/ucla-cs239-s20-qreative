b6304a98d3d9020b4cb9d4aa2dfa62daa3e45637
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-05-07 16:37:49 -0400

    Switched booleans to enums in dj.py for clarity

--- HW1/dj.py
+++ HW1/dj.py
@@ -1,14 +1,20 @@
+from enum import Enum
+
 import numpy as np
 import itertools
 import oracle
 
+class DJ(Enum):
+    CONSTANT = 0
+    BALANCED = 1
+
 # Unit Test Functions
 
-def is_bal_or_const(m, balanced):
-    if not balanced:
+def is_bal_or_const(m, f):
+    if f is DJ.CONSTANT:
         x = next(iter(m.values()))
         return all(val == x for val in m.values())
-    else:
+    elif f is DJ.BALANCED:
         val0 = sum(value == '0' for value in m.values())
         val1 = sum(value == '1' for value in m.values())
         return val0 == val1 and val0+val1 == len(m.values())
@@ -18,10 +24,10 @@ def is_unitary(m):
 
 def unit_tests(n):
     const_mapping = oracle.init_bit_mapping(n, dj=True, balanced=False)
-    assert is_bal_or_const(const_mapping, False)
+    assert is_bal_or_const(const_mapping, DJ.CONSTANT)
 
     balanced_mapping = oracle.init_bit_mapping(n, dj=True, balanced=True)
-    assert is_bal_or_const(balanced_mapping, True)
+    assert is_bal_or_const(balanced_mapping, DJ.BALANCED)
 
     U_const_f = oracle.gen_matrix(const_mapping, n, 1)
     assert is_unitary(U_const_f)
